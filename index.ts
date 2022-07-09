a4c5ba0e66dc02898d18c171cb2d2856f0a26e9a
Kyle Coberly <kyle.coberly@gmail.com>
2022-07-09 10:37:31 -0600

    Remove solution

--- index.ts
+++ index.ts
@@ -1,10 +1,3 @@
 export default function accumulate(string: string): string {
   return string
-    .split("")
-    .map(formatLetter)
-    .join("-")
-}
-
-function formatLetter(character: string, count: number) {
-  return `${character.toUpperCase()}${character.toLowerCase().repeat(count)}`
 }
